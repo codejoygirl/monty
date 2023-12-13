e6151fc1e70ab6adf5b50231f4d773be4a9188a7
codejoygirl <joyegbu@gmail.com>
2023-12-13 13:38:14 -0800

    nop

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- Nothing
+  *@head: The stack head
+  *@counter: Line_number
+  *Return: No return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}i
